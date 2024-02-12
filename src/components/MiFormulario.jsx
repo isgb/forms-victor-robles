8557b3a2c0e73200cba4d7da4773e7d7fc6e2985
isaias_pruebas <isgb_codigo@hotmail.com>
2024-02-11 19:34:20 -0600

    debug

--- src/components/MiFormulario.jsx
+++ src/components/MiFormulario.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import {useFormik} from 'formik'
 
 export const MiFormulario = () => {
+
+    const formik = useFormik({
+        initialValues:{ 
+            nombre: "",
+            email: ""
+        },
+        onSubmit: values => {
+            console.log(values)
+        }
+    })
   return (
-    <div>MiFormulario</div>
+    <div>
+        <h1>Mi formulario con formik</h1>
+
+        <form onSubmit={formik.handleSubmit}>
+            <div className='form-group'>
+                <label htmlFor="nombre">Nombre</label>
+                <input 
+                    type="text" id='nombre' name='nombre' 
+                    value={formik.values.nombre}
+                    onChange={formik.handleChange}
+                />
+            </div>
+
+            <div className='form-group'>
+                <label htmlFor="email">Email</label>
+                <input 
+                    type="text" id='email' name='email' 
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                />
+            </div>
+
+            <input type="submit" value={'Enviar'}/>
+        </form>
+    </div>
   )
 }
